d7f91ee2cfc830035bc6197d6d485934641c77bc
Michael <michel1darg@gmail.com>
2024-12-23 15:56:18 +0100

    mp4-video, music sound fix

--- pyton.py
+++ pyton.py
@@ -1,37 +1,38 @@
 import yt_dlp
 import os
 
-def download_playlist_as_mp3(playlist_url):
-    # Ścieżka do folderu z ffmpeg.exe
-    ffmpeg_path = r"C:\Users\fulek\Desktop\gigachad\ffmpeg-2024-12-19-git-494c961379-full_build\bin"
+def download_playlist_as_mp4(playlist_url):
+    # Путь к папке с ffmpeg.exe
+    ffmpeg_path = r"C:\Users\zbido\Downloads\ffmpeg-2024-12-19-git-494c961379-full_build\ffmpeg-2024-12-19-git-494c961379-full_build\bin"
 
-    # Sprawdź, czy ffmpeg.exe i ffprobe.exe znajdują się w podanej ścieżce
+    # Проверяем, что ffmpeg.exe и ffprobe.exe находятся в указанной папке
     if not all(os.path.exists(os.path.join(ffmpeg_path, exe)) for exe in ['ffmpeg.exe', 'ffprobe.exe']):
-        print("Nie znaleziono ffmpeg.exe lub ffprobe.exe w podanej ścieżce!")
+        print("Не удалось найти ffmpeg.exe или ffprobe.exe по указанному пути!")
         return
 
-    output_path = "downloads"  # Folder, w którym będą zapisane pliki
-    os.makedirs(output_path, exist_ok=True)  # Tworzy folder, jeśli nie istnieje
+    output_path = "Downloads"  # Папка для сохранения файлов
+    os.makedirs(output_path, exist_ok=True)  # Создаем папку, если ее нет
 
     ydl_opts = {
-        'format': 'bestaudio/best',
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
         'outtmpl': os.path.join(output_path, '%(playlist_index)s - %(title)s.%(ext)s'),
         'quiet': False,
-        'noplaylist': False,
-        'postprocessors': [
-            {
-                'key': 'FFmpegExtractAudio',
-                'preferredcodec': 'mp3',
-                'preferredquality': '192',
-            }
-        ],
-        'ffmpeg_location': ffmpeg_path,  # Dodajemy ścieżkę do ffmpeg.exe
+        'verbose': True,
+        'merge_output_format': 'mp4',
+        'ffmpeg_location': ffmpeg_path,
+        'postprocessors': [{'key': 'FFmpegMerger'}],
+        'ignoreerrors': True,  # Игнорировать ошибки
+        'retries': 2,  # Больше попыток
+        'playlist_items': '1-2',  # Ограничение на загрузку первых 10 видео
     }
 
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-        print(f"Pobieranie playlisty: {playlist_url}")
-        ydl.download([playlist_url])
+        try:
+            print(f"Загрузка плейлиста: {playlist_url}")
+            ydl.download([playlist_url])
+        except Exception as e:
+            print(f"Ошибка: {e}")
 
 if __name__ == "__main__":
-    playlist_url = input("Podaj URL playlisty YouTube: ")
-    download_playlist_as_mp3(playlist_url)
+    playlist_url = input("Введите URL плейлиста YouTube: ")
+    download_playlist_as_mp4(playlist_url)
